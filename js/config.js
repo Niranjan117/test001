dc83c9a1462af0ce93c484d1a6a46a5350c1d710
Niranjan Kandpal <191318166+Niranjan117@users.noreply.github.com>
2026-02-11 21:38:35 +0530

    Refactor CONFIG to use environment variables

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+  firebase: {
+    apiKey: window.__ENV__?.FIREBASE_API_KEY || "",
+    authDomain: window.__ENV__?.FIREBASE_AUTH_DOMAIN || "",
+    projectId: window.__ENV__?.FIREBASE_PROJECT_ID || "",
+    storageBucket: window.__ENV__?.FIREBASE_STORAGE_BUCKET || "",
+    messagingSenderId: window.__ENV__?.FIREBASE_MESSAGING_SENDER_ID || "",
+    appId: window.__ENV__?.FIREBASE_APP_ID || "",
+    measurementId: window.__ENV__?.FIREBASE_MEASUREMENT_ID || ""
+  },
+  backendApi: window.__ENV__?.BACKEND_API || "",
+  geminiKey: ""
+};
+
+export default CONFIG;
